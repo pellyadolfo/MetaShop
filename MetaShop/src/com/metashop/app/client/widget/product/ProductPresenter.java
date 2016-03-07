9311165e77fdf75d255d9c665a6c1fc7f5c36a63
pellyadolfo <pellyadolfo@pellyadolfo-EasyNote-TJ65>
2016-03-07 00:00:51 -0500

    replace composite product widget by Presenter Widget

---
+++ MetaShop/src/com/metashop/app/client/widget/product/ProductPresenter.java
@@ -0,0 +1,32 @@
+package com.metashop.app.client.widget.product;
+
+import com.google.inject.Inject;
+import com.google.web.bindery.event.shared.EventBus;
+import com.gwtplatform.mvp.client.HasUiHandlers;
+import com.gwtplatform.mvp.client.PresenterWidget;
+import com.gwtplatform.mvp.client.View;
+import com.metashop.app.data.Product;
+
+public class ProductPresenter extends PresenterWidget<ProductPresenter.MyView> implements ProductUiHandlers {
+ 
+    public interface MyView extends View, HasUiHandlers<ProductUiHandlers> {
+    	void setProduct(Product productVO, int slotsOf12);
+    }
+    
+    public void setProduct(Product product, int slotsOf12) {
+        getView().setProduct(product, slotsOf12);
+    }
+
+    @Inject
+    public ProductPresenter(final EventBus eventBus, final MyView view) {
+        super(eventBus, view);
+        
+        getView().setUiHandlers(this);
+    }
+    
+    @Override
+    public void showProductDetails(String name) {
+    	// TODO Auto-generated method stub
+    	
+    }
+}
